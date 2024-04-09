ac934ee94794f71cf452a1415a631440b56091f9
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-04-09 12:56:08 -0500

    feched weather and made card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
 const apiKey = '91ddb08cd7e973fd6416621b8fe4b84f';
+
+
+// Get the form element
+const cityForm = document.getElementById('cityform');
+
+// Add event listener for form submission
+cityForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const cityInput = document.getElementById('cityinput').value;
+    getWeather(cityInput);
+});
+
+// Function to fetch weather data
+const getWeather = (city) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+            getForecast(city);
+        });
+};
+
+const displayWeather = (data) => {
+  const weatherContainer = document.querySelector('.weathercontainer');
+  // weatherContainer.innerHTML = '';
+
+  const weatherCard = document.createElement('div');
+  weatherCard.classList.add('weathercard');
+
+  weatherCard.innerHTML = `
+      <h2>${data.name}</h2>
+      <div class="weatherdetails">
+          <p>Temperature: ${data.main.temp}°C</p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          <p>Wind Speed: ${data.wind.speed} m/s</p>
+      </div>
+  `;
+
+  weatherContainer.appendChild(weatherCard);
+};
